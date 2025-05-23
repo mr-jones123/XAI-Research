e6c5957f26ad106a4a65dc85d1c25e5e90d16e79
mercadoCODING <raphaelandremercado@gmail.com>
2025-05-23 23:38:53 +0800

    feat:added route changes

---
+++ src/app/api/chat_sessions/fetch/route.ts
@@ -0,0 +1,1 @@
+//TODO to para stored messages ssr fetching
--- src/app/api/chat_sessions/insert/route.ts
+++ src/app/api/chat_sessions/insert/route.ts
@@ -14,7 +14,6 @@ export async function POST(req: Request) {
     );
   }
 
-
   const { chatId: inputChatId } = await req.json();
   const chatId = inputChatId || uuidv4();
 
